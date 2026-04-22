e8a30a0df1e45a6f1db5592805c9bd0bbdfdb4f7
unknown <margaryanseryan@gmail.com>
2026-04-22 22:39:36 +0400

    final changs

--- server/models/ActivityLog.js
+++ server/models/ActivityLog.js
@@ -1,10 +1,14 @@
 // models/ActivityLog.js
 const mongoose = require("mongoose");
 
-const activitySchema = new mongoose.Schema({
-  actor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  action: String,
-  targetUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-}, { timestamps: true });
+const activitySchema = new mongoose.Schema(
+  {
+    actor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    action: String,
+    targetUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("ActivityLog", activitySchema);
+ 
